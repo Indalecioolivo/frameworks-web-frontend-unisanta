504da1ba2918e3a9808419cd3336ac79a7d701d1
Indalecio Olivo <indalecioolivo@gmail.com>
2025-02-15 13:14:39 -0300

    feat: implementado definições prevista na aula 02 de frameworks web.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Tarefa } from './tarefa';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TODOapp';
+
+  arrayDeTarefas: Tarefa[] = [];
+
+  constructor() {
+    this.READ_tarefas();
+  }
+
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.arrayDeTarefas.push(novaTarefa);
+  }
+
+  READ_tarefas() {
+    this.arrayDeTarefas = [
+      new Tarefa('Estudar Angular', false),
+      new Tarefa('Fazer TP Banco de dados', false),
+      new Tarefa('Documentos Estágio', false),
+    ];
+  }
 }
